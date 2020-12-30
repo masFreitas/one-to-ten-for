a2d5dac00a8d3a7ccff87ddebb10607317d57847
Mateus Arthur da Silva de Freitas <65245536+masFreitas@users.noreply.github.com>
2020-12-30 18:56:14 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ContaDeUmAteDez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // faca um algoritimo que mostre os numeros de 1 até 10
+            
+            for (int i = 0; i <= 10; i++)
+            
+            {
+                Console.Write("{0} ", i);
+            }
+        }
+    }
+}
